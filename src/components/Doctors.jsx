653049e3f962417e1c2f18aad1cf7691d8b08ed1
Pritesh Shinde <pritesh.s@purplle.com>
2022-11-27 17:16:31 +0530

    Doctors list filter functionality added

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,27 +1,81 @@
+import { useState } from 'react';
+
 import DoctorCard from './DoctorCard';
 import SectionHeading from './SectionHeading';
+
 import img1 from './doctor-1.jpeg';
 import img2 from './doctor-2.jpeg';
 import img3 from './doctor-3.jpeg';
 import img4 from './doctor-4.jpeg';
+
 import './Doctors.css';
 
 function Doctors() {
+  let [doctors] = useState([]);
+  let [filter, setFilter] = useState('');
+
+  doctors = [
+    {
+      image: img1,
+      name: 'Alice Grue',
+      designation: 'Psychiatrist'
+    },
+    {
+      image: img2,
+      name: 'Joseph Murphy',
+      designation: 'Cardiologist'
+    },
+    {
+      image: img3,
+      name: 'Alison Davis',
+      designation: 'Cardiologist'
+    },
+    {
+      image: img4,
+      name: 'Adam Taylor',
+      designation: 'Neurologist'
+    }
+  ];
+
+  let allDesignations = {
+    All: doctors.length
+  };
+  const designations = doctors.map(doctor => doctor.designation).reduce((acc, curr) => {
+    return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
+  }, {});
+  allDesignations = {...allDesignations, ...designations};
+
+  let designationArr = Object.keys(allDesignations);
+  let designationCount = Object.values(allDesignations);
+
+  let filterDesignations = (e) => {
+    designationArr.forEach(designation => {
+      document.getElementById(designation).classList.remove('active');
+    });
+    e.target.classList.add('active');
+    setFilter((e.target.id === 'All' ? '' : e.target.id));
+  };
+
   return (
     <section className={'doctors'}>
       <div className={'container'}>
         <SectionHeading heading={'Doctors'} description={'Et harum quidem rerum facilis est et expedita distinctio.'} />
         <ul className={'designation-nav'}>
-          <li><a className={'active'} href={'#All'}>All (4)</a></li>
-          <li><a href={'#Cardiologist'}>Cardiologist (2)</a></li>
-          <li><a href={'#Psychiatrist'}>Psychiatrist (1)</a></li>
-          <li><a href={'#Neurologist'}>Neurologist (1)</a></li>
+          {designationArr.map((designation, i, j) => {
+            return <li key={i} onClick={filterDesignations} id={designation} className={(filter === '' && designation === 'All') ? 'active' : ''} >{designation} ({designationCount[i]})</li>;
+          })}
         </ul>
         <div className={'doctor-cards'}>
-          <DoctorCard img={img1} name={'Alice Grue'} designation={'Psychiatrist'} />
-          <DoctorCard img={img2} name={'Joseph Murphy'} designation={'Cardiologist'} />
-          <DoctorCard img={img3} name={'Alison Davis'} designation={'Cardiologist'} />
-          <DoctorCard img={img4} name={'Adam Taylor'} designation={'Neurologist'} />
+          {
+            (filter === '') 
+              ? (doctors
+                .map((doctor, i, j) => <DoctorCard key={i} img={doctor.image} name={doctor.name} designation={doctor.designation} />)
+              )
+              : (doctors
+                .filter(doctor => doctor.designation === filter)
+                .map((doctor, i, j) => <DoctorCard key={i} img={doctor.image} name={doctor.name} designation={doctor.designation} />)
+              )
+          }
         </div>
       </div>
     </section>
